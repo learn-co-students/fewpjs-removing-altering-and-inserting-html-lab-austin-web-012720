29284772ad50a230c8838d2961aa298e2a60ed85
rixong <rixong@gmail.com>
2020-03-09 10:27:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+document.body.appendChild(newHeader)
+newHeader.innerHTML = "Rick is the champion"
