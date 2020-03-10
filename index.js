55b348ba57c902d51a6b516707b38348bc6bb210
github username <github email address>
2020-03-10 14:21:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+let main = document.getElementById('main')
+main.remove()
+
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Tom is the champion"
+
+
+
+// let element = document.createElement('div')
+
+// document.body.appendChild(element)
+
+// let ul = document.createElement('ul')
+
+// for (let i = 0; i < 4; i++) {
+//     let li = document.createElement('li')
+//     li.innerHTML = (i + 1).toString()
+//     ul.appendChild(li)
+// }
+// element.appendChild(ul)
