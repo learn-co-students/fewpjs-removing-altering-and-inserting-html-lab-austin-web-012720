abb5db0b32da246d68e7a4d5b68320db50e47fd7
benbrower <bbrower@smu.edu>
2020-08-17 18:01:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Write your code here!
-document.querySelector('main#main').remove();
 
-const newHeader = document.createElement('h1');
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
 newHeader.id = "victory";
 newHeader.innerHTML = "Ben is the champion";
-newHeader.class = "victory";
+
+// document.querySelector('main#main').remove();
+// const newHeader = document.createElement('h1');
+// newHeader.id = "victory";
+// newHeader.innerHTML = "Ben is the champion";
+// newHeader.class = "victory";
